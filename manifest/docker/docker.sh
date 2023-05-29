4bc60341b3f00aecc8565e4c612629c1be6a50ff
Li Dong <cnlidong@live.cn>
2023-05-29 20:31:51 +0800

    增加Docker构建脚本

--- manifest/docker/docker.sh
+++ manifest/docker/docker.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # This shell is executed before docker build.
+# 显示当前目录
+# 如果 frontend/dist 目录不存在,则编译前端
+if [ ! -d "frontend/dist" ]; then
+    echo "Building frontend ..."
+    cd frontend && yarn build && cd ..
+else
+    echo "Frontend build directory exists, skip building frontend"
+fi
+
 
 
 
---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+
+gf docker main.go -t xyhelper/chatgpt-mirror-server:latest
+# 修改镜像标签为当前日期时间
+time=$(date "+%Y%m%d%H%M%S")
+docker tag xyhelper/chatgpt-mirror-server:latest xyhelper/chatgpt-mirror-server:$time
+# 推送镜像到docker hub
+docker push xyhelper/chatgpt-mirror-server:latest
+docker push xyhelper/chatgpt-mirror-server:$time
