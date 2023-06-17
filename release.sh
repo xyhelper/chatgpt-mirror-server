df95a8d127421844e508c76f3757c9a54b4d8245
Li Dong <cnlidong@live.cn>
2023-06-17 21:58:57 +0800

    镜像发布通过阿里中转

--- release.sh
+++ release.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-
-gf docker main.go -t xyhelper/chatgpt-mirror-server:latest
+gf build main.go -a amd64 -s linux
+gf docker main.go -t registry.cn-beijing.aliyuncs.com/xyhelper/chatgpt-mirror-server:latest
 # 修改镜像标签为当前日期时间
 time=$(date "+%Y%m%d%H%M%S")
-docker tag xyhelper/chatgpt-mirror-server:latest xyhelper/chatgpt-mirror-server:$time
+docker tag registry.cn-beijing.aliyuncs.com/xyhelper/chatgpt-mirror-server:latest registry.cn-beijing.aliyuncs.com/xyhelper/chatgpt-mirror-server:$time
 # 推送镜像到docker hub
-docker push xyhelper/chatgpt-mirror-server:latest
-docker push xyhelper/chatgpt-mirror-server:$time
+docker push registry.cn-beijing.aliyuncs.com/xyhelper/chatgpt-mirror-server:latest
+docker push registry.cn-beijing.aliyuncs.com/xyhelper/chatgpt-mirror-server:$time
