4d4704d57f34eb78e1a89aa11e9afe03517c2072
GCSLaoLi <cnlidong@live.cn>
2024-02-17 10:19:14 +0800

    更新资源

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 # registry.cn-beijing.aliyuncs.com/
 set -e
 
-gf build main.go -a amd64 -s linux
+gf build main.go -a amd64 -s linux -p ./temp
 gf docker main.go -t xyhelper/chatgpt-mirror-server:latest
 # 修改镜像标签为当前日期时间
 time=$(date "+%Y%m%d%H%M%S")
